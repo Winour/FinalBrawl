71c688e6758a86313a4e124ae986667946b4681c
Dani Reyes <dani-kingdom@hotmail.com>
2018-05-14 23:24:27 +0200

    Add Json

---
+++ FinalBrawl/SaveTools.h
@@ -0,0 +1,17 @@
+#ifndef __SAVETOOLS_H__
+#define __SAVETOOLS_H__
+
+#include "nlohmann\json.hpp"
+
+class SaveTools
+{
+public:
+    SaveTools()
+    {
+    }
+
+private:
+
+};
+
+#endif // !__SAVETOOLS_H__
